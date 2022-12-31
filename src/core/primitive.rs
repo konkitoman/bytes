4305a581e9df8e460f42c24530774132d8b74dc2
Konkitoman <konkitoman@users.noreply.github.com>
2022-12-31 17:00:52 +0200

    Changelog 0.1.8:   * added type f32 and f64

--- src/core/primitive.rs
+++ src/core/primitive.rs
@@ -28,6 +28,54 @@ impl TBytes for bool {
     }
 }
 
+impl TBytes for f32 {
+    fn size(&self) -> usize {
+        4
+    }
+
+    fn to_bytes(&self) -> Vec<u8> {
+        self.to_le_bytes().to_vec()
+    }
+
+    fn from_bytes(buffer: &mut Vec<u8>) -> Option<Self>
+    where
+        Self: Sized,
+    {
+        Some(Self::from_le_bytes([
+            buffer.pop()?,
+            buffer.pop()?,
+            buffer.pop()?,
+            buffer.pop()?,
+        ]))
+    }
+}
+
+impl TBytes for f64 {
+    fn size(&self) -> usize {
+        8
+    }
+
+    fn to_bytes(&self) -> Vec<u8> {
+        self.to_le_bytes().to_vec()
+    }
+
+    fn from_bytes(buffer: &mut Vec<u8>) -> Option<Self>
+    where
+        Self: Sized,
+    {
+        Some(Self::from_le_bytes([
+            buffer.pop()?,
+            buffer.pop()?,
+            buffer.pop()?,
+            buffer.pop()?,
+            buffer.pop()?,
+            buffer.pop()?,
+            buffer.pop()?,
+            buffer.pop()?,
+        ]))
+    }
+}
+
 #[cfg(test)]
 mod test {
     use crate::TBytes;
@@ -51,5 +99,29 @@ mod test {
         let other = bool::from_bytes(&mut bytes).unwrap();
 
         assert_eq!(b, other)
+    }
+
+    #[test]
+    fn f32() {
+        let a = 5234.0f32;
+
+        let mut bytes = a.to_bytes();
+        bytes.reverse();
+
+        let other = f32::from_bytes(&mut bytes).unwrap();
+
+        assert_eq!(a, other)
+    }
+
+    #[test]
+    fn f64() {
+        let a = 43223.32f64;
+
+        let mut bytes = a.to_bytes();
+        bytes.reverse();
+
+        let other = f64::from_bytes(&mut bytes).unwrap();
+
+        assert_eq!(a, other)
     }
 }
